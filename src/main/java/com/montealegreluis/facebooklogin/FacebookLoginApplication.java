12c130c805a6c4c42d9235ce8476d0efb524329e
Luis Montealegre <montealegreluis@gmail.com>
2018-03-13 23:19:25 -0500

    chore: Create basic Spring Boot Web application

---
+++ src/main/java/com/montealegreluis/facebooklogin/FacebookLoginApplication.java
@@ -0,0 +1,12 @@
+package com.montealegreluis.facebooklogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacebookLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacebookLoginApplication.class, args);
+	}
+}
